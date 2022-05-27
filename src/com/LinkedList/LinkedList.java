15a55fc8545450c5c193184ab270b235c9691c02
Kondwani Ngulube <kondwanigideon.ngulube@gmail.com>
2022-05-27 13:14:51 +0200

    print linked list

--- src/com/LinkedList/LinkedList.java
+++ src/com/LinkedList/LinkedList.java
@@ -1,18 +1,50 @@
 package com.LinkedList;
 
+import com.company.Main;
+
+import java.util.List;
+
+//singly linked list
 public class LinkedList {
-    //generic Linked list
-    private  ListNode head; //head of the list
-    public static void main (String [] args) {
-        System.out.println("linked list implementation");
-    }
-    private static class ListNode{
-        private int data; // generic data type
-        private ListNode next;//its pointer to the next item on the list
-        //getter
-        public ListNode(int data){
-            this.data = data;
-            this.next = null; //the list only one has element so it only points to null
+    //declare a head
+    private static ListNode head;
+    private static class ListNode {
+        final int data; //head of list
+        private ListNode next; //pointer of list
+        //data = integer, pointer = next
+        public ListNode(int data) {
+            this.data = data;//set to the data we insert
+            this.next = null;// set to null
         }
+    }
+    //print the linked  list
+    public void display(){
+        //assigning a current node as the head of our list
+        ListNode current = head;
+        while(current != null) {
+            System.out.println(current.data + "--> ");
+            //ensuring that the current node traverses through each node until it reaches the node
+            // last pointer = null
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public static  void main (String [] args){
+        //ensure the l1 list is of our linked list main class
+        LinkedList l1 = new LinkedList();
+        //insert the head and its data
+        l1.head = new ListNode(2);
+        //insert data for other nodes
+        ListNode  second = new ListNode(4);
+        ListNode third = new ListNode(6);
+        ListNode fourth = new ListNode(8);
+        ListNode fifth = new ListNode(10);
+        //connect the pointers
+        l1.head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        l1.display();
+
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,10 @@
 package com.company;
+import com.LinkedList.LinkedList;
 
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
         System.out.println("main class");
     }
 }
